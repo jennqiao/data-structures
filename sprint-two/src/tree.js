2d4314a25bbff9c8aaa97daeb72dfb20d4d4c4c8
jennqiao <jennifercqiao@gmail.com>
2018-03-07 17:38:22 -0800

    Add pseudocode for tree.contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,6 +15,22 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  // Set flag = false
+  // Initial node is the tree with this method ('this')
+  // If the node has the target value
+  //  return true
+  // If it has children
+  //  loop over children
+  //    flag = recurse on child
+  //    if true, break
+  //  return false
+  //                reduce
+  //                  if true
+  //                    return true
+  //                  memo = recurse on each child
+  //                  accumulator: false
+  // Else if it doesn't have target value
+  //  return false
 };
 
 
