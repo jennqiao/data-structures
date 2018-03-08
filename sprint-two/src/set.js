1182e5ad373ff51b5dc702bc8660925dc3142cfd
jennqiao <jennifercqiao@gmail.com>
2018-03-08 13:54:18 -0800

    Implement set data structure

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,15 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return Boolean(this[item]);
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
