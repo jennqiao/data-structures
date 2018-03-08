43b1de81dceab5fb1682596f954c048ae652fac9
jennqiao <jennifercqiao@gmail.com>
2018-03-07 18:25:43 -0800

    Finish tree.contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,6 +15,20 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  var hasFound = false;
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      hasFound = this.children[i].contains(target);
+      if (hasFound) {
+        return true;
+      }
+    }
+    return false;
+  } else {
+    return false;
+  }
   // Set flag = false
   // Initial node is the tree with this method ('this')
   // If the node has the target value
@@ -22,7 +36,7 @@ treeMethods.contains = function(target) {
   // If it has children
   //  loop over children
   //    flag = recurse on child
-  //    if true, break
+  //    if true, return
   //  return false
   //                reduce
   //                  if true
