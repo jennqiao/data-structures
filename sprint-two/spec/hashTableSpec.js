51fd802fee45cd88efb82154721d7690cb7fd7de
jennqiao <jennifercqiao@gmail.com>
2018-03-09 17:50:32 -0800

    resize works now for hash tables

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -48,13 +48,13 @@ describe('hashTable', function() {
   });
 
   it('should not use plain objects', function(){
-    hashTable.insert('Steven', 'Tyler')
+    hashTable.insert('Steven', 'Tyler');
     var index = getIndexBelowMaxForKey('Steven', hashTable._limit);
     expect(typeof hashTable[index] === 'object' && !Array.isArray(hashTable[index])).to.equal(false);
   });
 
   // (Advanced! Remove the extra "x" when you want the following tests to run)
-  xit ('should double in size when needed', function() {
+  it ('should double in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
@@ -64,7 +64,7 @@ describe('hashTable', function() {
     expect(hashTable._limit).to.equal(16);
   });
 
-  xit ('should halve in size when needed', function() {
+  it ('should halve in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,47 +1,82 @@
 
 
-var HashTable = function() {
-  this._limit = 8;
+var HashTable = function(limit) {
+  this._limit = limit || 8;
   this._storage = LimitedArray(this._limit);
+  this._counter = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if(!this[index]){
-    this[index] = [];
+  if(!this._storage.get(index)){
+    this._storage.set(index, []);
   } 
   var hasKey = false;
-  for (var i = 0; i < this[index].length; i++) {
-    if (this[index][i][0] === k) {
-      this[index][i][1] = v;
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index)[i][1] = v;
       hasKey = true; // Alt: use return instead of flag
     }
   }
   if(!hasKey){ // And then you could delete the if here
-    this[index].push([k, v]);
+    this._storage.get(index).push([k, v]);
+    this._counter++;
+    if(this._counter > this._limit*.75) {
+      this.resizeHash(2);
+    }
+
+  
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for(var i = 0; i < this[index].length; i++){
-    if(this[index][i][0] === k){
-      return this[index][i][1];
+  if (this._storage.get(index)) {
+    for(var i = 0; i < this._storage.get(index).length; i++){
+      if(this._storage.get(index)[i][0] === k){
+        return this._storage.get(index)[i][1];
+      }
     }
-  }
+  } 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for(var i = 0; i < this[index].length; i++){
-    if(this[index][i][0] === k){
-      this[index].splice(i, 1);
+  if (this._storage.get(index)){
+    for(var i = 0; i < this._storage.get(index).length; i++){
+      if(this._storage.get(index)[i][0] === k){
+        this._storage.get(index).splice(i, 1);
+        this._counter--;
+      }
     }
   }
+  if(this._counter < this._limit*.25 && this._counter !== 0) {
+    this.resizeHash(0.5);
+  }
+
 };
 
+HashTable.prototype.resizeHash = function(num) {
+  
+  this._limit = this._limit*num;
+  var oldHashStorage = this._storage;
+  var newHashTable = new HashTable(this._limit);
 
+  var rehash = function(bucket, bucketIndex) {
+    if (bucket) {
+      for (var i=0; i<bucket.length; i++) {
+        var key = bucket[i][0];
+        var value = bucket[i][1];
+        newHashTable.insert(key, value);
+      }
+    }
+  };
 
+  oldHashStorage.each(rehash);
+  this._storage = newHashTable._storage;
+  this._counter = newHashTable._counter;
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
