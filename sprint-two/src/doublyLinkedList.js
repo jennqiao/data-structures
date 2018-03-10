93d9109c3c780600ed78dca7a4ebad15b4e8df9b
jennqiao <jennifercqiao@gmail.com>
2018-03-09 20:03:32 -0800

    Implement doublyLinkedList with some additional tests

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -1,4 +1,4 @@
-var doublyLinkedList = function() {
+var DoublyLinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
@@ -7,11 +7,63 @@ var doublyLinkedList = function() {
 };
 
 var doublyLinkedListMethods = {
-  addToHead: function() {},
-  removeHead: function() {},
-  addToTail: function() {},
-  removeTail: function() {},
-  contains: function() {}
+  addToHead: function(value) {
+    var node = Node(value);
+    var oldHead = this.head;
+    this.head = node;
+    if(oldHead){
+      oldHead.previous = node;
+      node.next = oldHead;
+    } else {
+      this.tail = node;
+    }
+  },
+  removeHead: function() {
+    if(!this.head){ // No nodes in list
+      return null;
+    } else if (!this.head.next){ // One node in list
+      var value = this.head.value;
+      this.head = null;
+      this.tail = null;
+      return value;
+    } else { // 2+ nodes
+      var oldHead = this.head;
+      this.head = oldHead.next;
+      this.head.previous = null;
+      return oldHead.value;
+    }
+  },
+  addToTail: function(value) {
+    var node = Node(value);
+    var oldTail = this.tail;
+    this.tail = node;
+    if(oldTail){
+      oldTail.next = node;
+      node.previous = oldTail;
+    } else {
+      this.head = node;
+    }
+  },
+  removeTail: function() {
+    if(this.tail){
+      var oldTail = this.tail;
+      this.tail = oldTail.previous; // Previous, or null if only one
+      if(this.tail){ // If there's nodes left
+        this.tail.next = null;
+      }
+      return oldTail.value;
+    }
+  },
+  contains: function(target) {
+    node = arguments[1] || this.head;
+    if(node.value === target){
+      return true;
+    } else if (!node.next){
+      return false;
+    } else {
+      return this.contains(target, node.next);
+    }
+  }
 };
 
 var Node = function(value) {
